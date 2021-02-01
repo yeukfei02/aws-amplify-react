73f37336856e495e4c616862c0203da360eb5cc3
Donald Wu <yeukfei02@gmail.com>
2021-02-01 15:49:06 +0800

    feat: 🎸 create aws-amplify-react project

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # aws-amplify-react
-aws-amplify-react
+
+This project wants to use `aws-amplify` to create a full-stack web project.
+
+`api`: graphql, amplify will generate `query/mutation/subscription` based on `schema.graphql`, deploy with `aws appsync`
+
+`db`: dynaomodb, amplify will create table based on `schema.graphql`
+
+`web`: react, will use `graphql` endpoint
+
+url:
+
+## Requirement:
+
+- install yarn
+- install node (v12+)
+- install aws amplify cli
+
+## Testing and run:
+
+```
+// install node dependencies
+$ yarn
+
+// test in local
+$ yarn run start
+
+// use amplify cli, use help to see what command can use
+$ amplify --help
+
+amplify <command> <subcommand>
+
+  init                Initializes a new project, sets up deployment resources in the cloud, and makes your project ready for Amplify.
+  configure           Configures the attributes of your project for amplify-cli, such as switching front-end framework and adding/removing cloud-provider plugins.
+  push                Provisions cloud resources with the latest local developments.
+  pull                Fetch upstream backend environment definition changes from the cloud and updates the local environment to match that definition.
+  publish             Executes amplify push, and then builds and publishes client-side application for hosting.
+  serve               Executes amplify push, and then executes the project's start command to test run the client-side application locally.
+  status              Shows the state of local resources not yet pushed to the cloud (Create/Update/Delete).
+  delete              Deletes all of the resources tied to the project from the cloud.
+  <category> add      Adds a resource for an Amplify category in your local backend
+  <category> update   Update resource for an Amplify category in your local backend.
+  <category> push     Provisions all cloud resources in a category with the latest local developments.
+  <category> remove   Removes a resource for an Amplify category in your local backend.
+  <category>          Displays subcommands of the specified Amplify category.
+  mock                Run mock server for testing categories locally.
+  codegen             Generates GraphQL statements(queries, mutations and eventHandlers) and type annotations.
+  env                 Displays and manages environment related information for your Amplify project.
+  console             Opens the web console for the selected cloud resource.
+  logout              If using temporary cloud provider credentials, this logs out of the account.
+```
---
+++ src/components/App.js
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import moment from "moment";
+
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import { createNotes, deleteNotes } from "../graphql/mutations";
+import { listNotess } from "../graphql/queries";
+
+import awsExports from "../aws-exports";
+Amplify.configure(awsExports);
+
+function App() {
+  const [name, setName] = useState([]);
+  const [description, setDescription] = useState([]);
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = async () => {
+    const listNotesData = await API.graphql(graphqlOperation(listNotess));
+    const notes = listNotesData.data.listNotess.items;
+    console.log("notes = ", notes);
+
+    if (notes) {
+      setNotes(notes);
+    }
+  };
+
+  const handleNameChange = (e) => {
+    if (e.target.value) {
+      setName(e.target.value);
+    }
+  };
+
+  const handleDescriptionChange = (e) => {
+    if (e.target.value) {
+      setDescription(e.target.value);
+    }
+  };
+
+  const handleSubmitClick = async (name, description) => {
+    if (name && description) {
+      const data = {
+        input: {
+          id: nanoid(),
+          name: name,
+          description: description,
+        },
+      };
+      await API.graphql(graphqlOperation(createNotes, data));
+      await getNotes();
+    }
+  };
+
+  const handleDeleteClick = async (id) => {
+    if (id) {
+      const data = {
+        input: {
+          id: id,
+        },
+      };
+      await API.graphql(graphqlOperation(deleteNotes, data));
+      await getNotes();
+    }
+  };
+
+  const renderNotesList = (notes) => {
+    let notesListDiv = null;
+
+    if (notes) {
+      notesListDiv = notes.map((item, i) => {
+        return (
+          <div className="card my-3" key={i}>
+            <div className="card-body">
+              <div>Name: {item.name}</div>
+              <div>Description: {item.description}</div>
+              <div>
+                CreatedAt:{" "}
+                {moment(item.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+              </div>
+              <div>
+                UpdatedAt:{" "}
+                {moment(item.updatedAt).format("YYYY-MM-DD HH:mm:ss")}
+              </div>
+              <div className="mt-3">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => handleDeleteClick(item.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      });
+    }
+
+    return notesListDiv;
+  };
+
+  return (
+    <div className="container p-5">
+      <h3 className="">Notes taking app</h3>
+
+      <div>
+        <div className="my-3">
+          <label className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="name"
+            onChange={(e) => handleNameChange(e)}
+          />
+        </div>
+
+        <div className="my-3">
+          <label className="form-label">Description</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="description"
+            onChange={(e) => handleDescriptionChange(e)}
+          />
+        </div>
+
+        <div className="my-3">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => handleSubmitClick(name, description)}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+
+      <div>{renderNotesList(notes)}</div>
+    </div>
+  );
+}
+
+export default App;
